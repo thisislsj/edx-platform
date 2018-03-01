ad16f795dac297ec51ce61f3b4f403317695a561
Michael LoTurco <mikeloturco13@gmail.com>
2018-03-01 14:18:04 -0500

    change functions to take objects as params

--- lms/djangoapps/support/static/support/jsx/entitlements/data/api/client.js
+++ lms/djangoapps/support/static/support/jsx/entitlements/data/api/client.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 
 import endpoints from './endpoints';
 
-export function requestEntitlements(usernameOrEmail) {
+export function requestEntitlements({usernameOrEmail}) {
   return fetch(
     `${endpoints.entitlementList}/${usernameOrEmail}`, {
       credentials: 'same-origin',
@@ -12,7 +12,7 @@ export function requestEntitlements(usernameOrEmail) {
   );
 }
 
-export function createEntitlement(courseUuid, user, mode, reason, comments) {
+export function createEntitlement({courseUuid, user, mode, reason, comments}) {
   return fetch(
     `${endpoints.entitlementList}/${user}`, {
       credentials: 'same-origin',
@@ -33,7 +33,7 @@ export function createEntitlement(courseUuid, user, mode, reason, comments) {
   );
 }
 
-export function updateEntitlement(email, reason, entitlementUuid, comments) {
+export function updateEntitlement({email, reason, entitlementUuid, comments}) {
   //Only requires an 'email' parameter to construct the url, not actually sent in the body
   return fetch(
     `${endpoints.entitlementList}/${email}`, {
