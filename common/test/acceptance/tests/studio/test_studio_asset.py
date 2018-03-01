209929b29c1b00f9029b60497336ca40aff5300d
Farhanah Sheets <fsheets@edx.org>
2018-03-01 12:08:43 -0500

    Remove old assets page tests

--- common/test/acceptance/tests/studio/test_studio_asset.py
+++ common/test/acceptance/tests/studio/test_studio_asset.py
@@ -11,51 +11,6 @@ from common.test.acceptance.tests.studio.base_studio_test import StudioCourseTes
 from common.test.acceptance.pages.studio.asset_index import UPLOAD_FILE_DIR
 
 
-class AssetIndexTest(StudioCourseTest):
-    """
-    Tests for the Asset index page.
-    """
-
-    def setUp(self, is_staff=False):  # pylint: disable=arguments-differ
-        super(AssetIndexTest, self).setUp()
-        self.asset_page = AssetIndexPage(
-            self.browser,
-            self.course_info['org'],
-            self.course_info['number'],
-            self.course_info['run']
-        )
-
-    def populate_course_fixture(self, course_fixture):
-        """
-        Populate the children of the test course fixture.
-        """
-        ConfigModelFixture('/config/assets', {'enabled_for_all_courses': False, 'enabled': False}, 'cms').install()
-        self.course_fixture.add_asset(['image.jpg', 'textbook.pdf'])
-
-    @skip_if_browser('chrome')  # TODO Need to fix test_page_existance for this for chrome browser
-    def test_type_filter_exists(self):
-        """
-        Make sure type filter is on the page.
-        """
-        self.asset_page.visit()
-        assert self.asset_page.type_filter_on_page() is True
-
-    @skip_if_browser('chrome')  # TODO Need to fix test_page_existance for this for chrome browser
-    def test_filter_results(self):
-        """
-        Make sure type filter actually filters the results.
-        """
-        self.asset_page.visit()
-        all_results = len(self.asset_page.return_results_set())
-        if self.asset_page.select_type_filter(1):
-            filtered_results = len(self.asset_page.return_results_set())
-            assert self.asset_page.type_filter_header_label_visible()
-            assert all_results > filtered_results
-        else:
-            msg = "Could not open select Type filter"
-            raise StudioApiLoginError(msg)
-
-
 class AssetIndexTestStudioFrontend(StudioCourseTest):
     """Tests for the Asset index page."""
 
@@ -70,7 +25,6 @@ class AssetIndexTestStudioFrontend(StudioCourseTest):
 
     def populate_course_fixture(self, course_fixture):
         """Populate the children of the test course fixture."""
-        ConfigModelFixture('/config/assets', {'enabled_for_all_courses': True, 'enabled': True}, 'cms').install()
         self.course_fixture.add_asset(['image.jpg', 'textbook.pdf'])
 
     def test_page_with_assets_elements_load(self):
